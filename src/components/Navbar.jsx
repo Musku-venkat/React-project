1ce9c72c0324a9471a58e4d0cb8ef49cac6b1708
Musku-venkat <muskuvenkat31@gmail.com>
2025-07-04 10:20:37 +0530

    fourth commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,40 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark px-3 m-2 rounded-2" style={{backgroundColor:'#5EABD6'}}>
-      <Link className="navbar-brand nav-link active" to={''}><b>CricAlerts</b></Link>
-      <div className="collapse navbar-collapse">
-         <ul className="navbar-nav ms-auto">
+    <nav className="navbar navbar-expand-lg navbar-dark px-2 m-2 rounded-2" style={{ backgroundColor: '#5EABD6' }}>
+      <div className="container-fluid d-flex justify-content-between align-items-center w-100">
+
+        <div className='d-sm-none d-block text-center'>
+          {/* <span className="bi bi-list text-light fs-1 py-0 btn"></span> */}
+              <div className="dropdown mt-3">
+                <button className="btn dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                  <span className="bi bi-list text-light fs-1 py-0 btn"></span>
+                </button>
+                <ul className="dropdown-menu">
+                  <li><Link className="dropdown-item" href="#">Action</Link></li>
+                  <li><Link className="dropdown-item" href="#">Another action</Link></li>
+                  <li><Link className="dropdown-item" href="#">Something else here</Link></li>
+                </ul>
+              </div>
+        </div>
+
+        <div>
+          <Link className="navbar-brand nav-link active" to={''}><b>CricAlerts</b></Link>
+        </div>
+
+        <div className="d-none d-sm-flex justify-content-center">
+          <ul className="navbar-nav flex-row gap-4 ms-auto">
             <li className="nav-item"><Link className="nav-link text-white" to={"/"}>Live</Link></li>
             <li className="nav-item"><Link className="nav-link text-white" to={"/schedule"}>Matches</Link></li>
             <li className="nav-item"><Link className="nav-link text-white" to={"/news"}>News</Link></li>
             <li className="nav-item"><Link className="nav-link text-white" to={"/about"}>About</Link></li>
-         </ul>
-      </div>
-      <div>
-        <Link className='text-white text-decoration-none' to={'/login'}>Login</Link>
+          </ul>
+        </div>
+
+        <div>
+          <Link className="text-white text-decoration-none" title='Login' to={'/login'}><span className="bi bi-person-circle fs-4"></span></Link>
+        </div>
+
       </div>
     </nav>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,19 +3,21 @@ import { Link } from 'react-router-dom';
 function Register(){
     return(
         <>
-            <form className="shadow rounded-2 p-2 text-center">
-                <h2 className='m-4'>Sign Up</h2>
-                <div className="form-floating mb-2">
-                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                    <label htmlFor="floatingInput">Email address</label>
-                </div>
-                <div className="form-floating mb-4">
-                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-                    <label htmlFor="floatingPassword">Password</label>
-                </div>
-                <button className='btn btn-primary mb-4 px-4 py-2'>Sign Up</button>
-                <p>Already have an account? <Link to={'/login'}> Log In</Link></p>
-            </form>
+            <div className="d-flex justify-content-center align-items-center">
+                <form className="shadow-lg rounded-4 p-5 text-center ">
+                    <h2 className='mb-4'>Sign Up</h2>
+                    <div className="form-floating mb-4">
+                        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" required/>
+                        <label htmlFor="floatingInput">Email address</label>
+                    </div>
+                    <div className="form-floating mb-4">
+                        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" required/>
+                        <label htmlFor="floatingPassword">Password</label>
+                    </div>
+                    <button className='btn btn-primary mb-4 px-4 py-2'>Sign Up</button>
+                    <p>Already have an account? <Link to={'/login'}> Log In</Link></p>
+                </form>
+            </div>
         </>
     )
 }
